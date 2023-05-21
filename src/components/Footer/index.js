f642cc9064a0a81b51dda9d35169281821a557e6
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-05-21 01:11:38 -0700

    Add react imports and plans for each components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,11 @@
 /*
+This is set at the bottom of the website
+Things inside the footer
 
+Home (Label, no links)
+    - About me (link)
+    - Project (link)
+    - Resume (link)
+    - Contact (link)
 */
 import React from 'react';
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,9 @@
 /*
-
+Navigation bar will have
+About me | Portfolio | Contact | Resume
 */
 import React from 'react';
+
+function Navigation (props){
+    
+}
