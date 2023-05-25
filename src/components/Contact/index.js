2b7e877fd5e2cf70556988e46f71023bf2d43797
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-05-24 20:24:11 -0700

    Added helper for contact file to validate email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,41 +4,80 @@ This is for the contact page.
 */
 
 import React, { useState } from 'react';
-//import '../../../';
+import validateEmail from '../../utils/helpers';
 
 function Form() {
-    const [userName, setUserName] = useState('');
-  
-    const handleInputChange = () => {
-      return setUserName(userName);
+    const [formData, setFormData] = useState({
+      userName: "",
+      email: "",
+      message: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { userName, email, message } = formData;
+
+    const handleInputChange = (e) => {
+      const { target } = e;
+      const inputType = target.name;
+      const inputValue = target.value;
+      if (inputType === "email") {
+        setFormData({...formData, [inputType]:inputValue});
+      }else{
+        alert("You didn't complete the form");
+      }
     };
-  
+
     const handleFormSubmit = (e) => {
-      // Preventing the default behavior of the form submit (which is to refresh the page)
       e.preventDefault();
-  
-      // Alert the user their username, clear the inputs
+
+      if (!validateEmail(email) || !userName) {
+        setErrorMessage('Email or username is invalid');
+        return;
+        
+      }
+      
       alert(`Hello ${userName}`);
-      setUserName('');
+
+      setFormData({
+        userName: "",
+        email: "",
+        message: "",
+      });
+      
     };
-  
+
     return (
       <div>
-        <p>
-          Hello {userName}
-        </p>
+        <p>Hello {userName}</p>
         <form className="form">
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
           <input
             value={userName}
             name="userName"
             onChange={handleInputChange}
             type="text"
-            placeholder="Username"
+            placeholder="username"
           />
-          <button type="button" onClick={handleFormSubmit}>
-            Submit
-          </button>
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="Message"
+          />
+          <button type="button" onClick={handleFormSubmit}>Submit</button>
         </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
       </div>
     );
   }
